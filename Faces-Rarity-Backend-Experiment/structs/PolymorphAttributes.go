789fa5086c701cfa9749321ed4263c7a19796ade
Nick Kirov <nik.kirov@limechain.tech>
2022-08-10 17:29:27 +0300

    init repo with polygon bridge integration

---
+++ Faces-Rarity-Backend-Experiment/structs/PolymorphAttributes.go
@@ -0,0 +1,17 @@
+package structs
+
+type PolymorphAttributes struct {
+	Background       string
+	HairLeft         string
+	HairRight        string
+	EarLeft          string
+	EarRight         string
+	BeardTopLeft     string
+	BeardTopRight    string
+	BeardBottomLeft  string
+	BeardBottomRight string
+	LipsLeft         string
+	LipsRight        string
+	EyeRight         string
+	EyeLeft          string
+}
